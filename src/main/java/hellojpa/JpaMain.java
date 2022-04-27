646b58f5b1dcbff6b8b5a12fc1950662cbcbeca4
eeun <eeun95@gmail.com>
2022-04-27 17:56:03 +0900

    JPA 시작하기

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,63 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JpaMain {
+    public static void main(String[] args) {
+
+        // application 로딩 시점에 딱 하나만 만듦
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            //code (db에 데이터를 저장한다거나 불러온다거나)
+
+            /*Member member = new Member();
+            member.setId(2L);
+            member.setName("HelloB");
+
+             */
+
+            Member findMember = em.find(Member.class, 1L);
+            System.out.println(findMember.getId() + " " + findMember.getName());
+            //findMember.setName("HelloJPA");
+
+            // JPQL 은 테이블을 대상으로 쿼리를 짜지 않음 Entity 객체를 대상으로 가져옴
+            List<Member> result = em.createQuery("select m from Member as m where m.name='HelloB'", Member.class)
+                    //.setFirstResult(1)    pagenation  1번부터
+                    //.setMaxResults(10)    pagenation  10개가져와
+                    .getResultList();
+
+            for (Member member : result) {
+                System.out.println(member.getName());
+            }
+
+            // java 컬렉션을 다루는 것처럼 설계됐기 때문에 수정 시 따로 persist 안해줘도됨
+
+            /*
+            삽입
+            em.persist(member);
+             */
+            /*
+            삭제
+            em.remove(findMember);
+             */
+
+            tx.commit();
+        } catch(Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
